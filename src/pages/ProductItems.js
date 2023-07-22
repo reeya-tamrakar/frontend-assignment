ef06c25f83b61f6aef59da27da571a8399ec15cb
reeya-tamrakar <reeya6792@gmail.com>
2023-07-22 11:48:52 +0545

    'msg'

--- src/pages/ProductItems.js
+++ src/pages/ProductItems.js
@@ -169,7 +169,7 @@ export default function ProductItems() {
                   ${product.price}
                 </span>
                 <button className="flex ml-auto text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">
-                  Buttonn
+                  Button
                 </button>
                 <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
                   <svg
