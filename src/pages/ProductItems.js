825f4782ac51b380e2743fc34a31d389d921e75b
reeya-tamrakar <reeya6792@gmail.com>
2023-07-22 11:37:57 +0545

    'msg'

--- src/pages/ProductItems.js
+++ src/pages/ProductItems.js
@@ -131,9 +131,7 @@ export default function ProductItems() {
                   </a>
                 </span>
               </div>
-              <p className="leading-relaxed">
-               {product.description}
-              </p>
+              <p className="leading-relaxed">{product.description}</p>
               <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
                 <div className="flex">
                   <span className="mr-3">Color</span>
@@ -171,7 +169,7 @@ export default function ProductItems() {
                   ${product.price}
                 </span>
                 <button className="flex ml-auto text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">
-                  Button
+                  Buttonn
                 </button>
                 <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
                   <svg
